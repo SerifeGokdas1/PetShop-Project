de38819e4b46d4d9901030d39b475e5b7fb543f3
SerifeGokdas1 <serifegokdaas@gmail.com>
2023-02-22 16:11:14 +0300

    Add project files.

---
+++ PetShop.Data/Migrations/20221227131157_UpdateDb.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PetShop.Data.Migrations
+{
+    public partial class UpdateDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "VolunteerPhone",
+                table: "Volunteers",
+                maxLength: 11,
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldMaxLength: 11);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AdopterPhone",
+                table: "Adopters",
+                maxLength: 11,
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldMaxLength: 11);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "VolunteerPhone",
+                table: "Volunteers",
+                type: "int",
+                maxLength: 11,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 11);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "AdopterPhone",
+                table: "Adopters",
+                type: "int",
+                maxLength: 11,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 11);
+        }
+    }
+}
